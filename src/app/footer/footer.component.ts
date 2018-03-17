740b854736d8063b0cffa6e4d8cf0ce3f4fb91a5
Robert Raboud <robert.raboud@randreng.com>
2018-03-17 19:08:13 -0400

    more configurable

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigurationService } from '../shared/services/configuration.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  name: string = '';
+  address1: string = '';
+  address2: string = '';
+  city_state_zip: string = '';
+  phone: string = '';
+  fax: string = '';
 
-  constructor() { }
+  constructor(private config: ConfigurationService) {
+    this.config.load().subscribe(x => {
+      this.name = this.config.serverSettings.Name;
+      this.address1 = this.config.serverSettings.Address1;
+      this.address2 = this.config.serverSettings.Address2;
+      this.city_state_zip = this.config.serverSettings.City;
+      this.city_state_zip += ', ' + this.config.serverSettings.State;
+      this.city_state_zip += ' ' + this.config.serverSettings.Zip;
+      this.phone = this.config.serverSettings.Phone;
+      this.fax = this.config.serverSettings.Fax;
+  });
+}
 
   ngOnInit() {
   }
--- src/app/shared/models/configuration.model.ts
+++ src/app/shared/models/configuration.model.ts
@@ -5,4 +5,12 @@ export interface IConfiguration {
     basketUrl: string;
     marketingUrl: string;
     activateCampaignDetailFunction: boolean;
-}
+    Name: string;
+    Phone: string;
+    Fax: string;
+    Address1: string;
+    Address2: string;
+    City: string;
+    State: string;
+    Zip: string;
+  }
